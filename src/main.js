b7f12568a772deafd45e7071fedad62d8ac1b597
jdav892 <jadavila9@gmail.com>
2024-10-19 16:48:28 -0400

    creates animation loops for rings and motobugs

--- src/main.js
+++ src/main.js
@@ -6,8 +6,24 @@ k.loadSprite("sonic", "graphics/sonic.png", {
     //Slicing sprite graphics by row/column
     sliceX: 8,
     sliceY: 2,
-    anims: {
+    animations: {
         run: {from: 0, to: 7, loop: true, speed: 30},
         jump: {from: 8, to : 15, loop: true, speed: 100},
+    }
+});
+
+k.loadSprite("ring", "graphics/ring.png", {
+    sliceX: 16,
+    sliceY: 1,
+    animations: {
+        rotation: {from: 0, to: 15, loop: true, speed: 20},
+    }
+});
+
+k.loadSprite("motobug", "graphics/motobug.png", {
+    sliceX: 5,
+    sliceY: 1,
+    animations: {
+        run: {from: 0, to: 4, loop: true, speed: 8}
     }
 });
