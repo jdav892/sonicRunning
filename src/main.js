7632fbb31de5a3f5e2b3fadfd1dc7c4ee4f4224a
jdav892 <jadavila9@gmail.com>
2024-10-19 16:44:20 -0400

    creates objects that stores animation frame slices for running and jumping

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import k from "./kaplayContext"
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
 k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+    //Slicing sprite graphics by row/column
+    sliceX: 8,
+    sliceY: 2,
+    anims: {
+        run: {from: 0, to: 7, loop: true, speed: 30},
+        jump: {from: 8, to : 15, loop: true, speed: 100},
+    }
+});
