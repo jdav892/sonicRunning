84f6e6df32290d9db6c3b68c7b301394622f4a45
jdav892 <jadavila9@gmail.com>
2024-10-21 03:07:51 -0400

    fixes error to initialize Rect constructor to create new shape for bugs

---
+++ src/entities/motobug.js
@@ -0,0 +1,15 @@
+import k from "../kaplayContext";
+
+
+//Did not define as default to allow for more entities to be created
+export function makeMotobug(pos) {
+    return k.add([
+        k.sprite("motobug", { anim: "run"}),
+        k.area({ shape: new k.Rect(k.vec2(-5, 0), 32, 32) }),
+        k.scale(4),
+        k.anchor("center"),
+        k.pos(pos),
+        k.offscreen(),
+        "enemy",
+    ])
+}
